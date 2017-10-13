9265b702524f7ddb74b312226436fb688946e305
Minh Ha <vanminh0910@gmail.com>
2017-10-14 00:21:56 +0700

    Fix bug when disconnect

--- firmware/on_blocky_disconnected.lua
+++ firmware/on_blocky_disconnected.lua
@@ -3,7 +3,12 @@ if connectBlinkTimer ~= nil then
   connectBlinkTimer = nil
   collectgarbage()
 end
-startConnectingBlink()
+-- start connecting blink
+connectBlinkTimer = tmr.create()
+connectBlinkTimer:alarm(300, tmr.ALARM_AUTO, function (t)
+  if (statusLedOn == 1) then gpio.write(4, gpio.LOW) else gpio.write(4, gpio.HIGH) end
+  statusLedOn = 1 - statusLedOn
+end)
 reconnectTimer = tmr.create()
 reconnectTimer:alarm(10000, tmr.ALARM_AUTO, function (t)
   blocky.connect()
