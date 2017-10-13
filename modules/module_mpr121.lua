caedc190507306f8166494fb027ef86117cc40a7
curlyz <curly.saigonese@gmail.com>
2017-10-13 22:37:19 +0700

    Update module_mpr121.lua

--- modules/module_mpr121.lua
+++ modules/module_mpr121.lua
@@ -1,47 +1,9 @@
-
-
+--FileName	MPR121
+--Version	1
+--Contributor	holycow
 local MPR121 = {}
-local codes = {
-    MPR121_I2CADDR_DEFAULT = 0x5A,
-	MPR121_TOUCHSTATUS_L   = 0x00,
-	MPR121_TOUCHSTATUS_H   = 0x01,
-	MPR121_FILTDATA_0L     = 0x04,
-	MPR121_FILTDATA_0H     = 0x05,
-	MPR121_BASELINE_0      = 0x1E,
-	MPR121_MHDR            = 0x2B,
-	MPR121_NHDR            = 0x2C,
-	MPR121_NCLR            = 0x2D,
-	MPR121_FDLR            = 0x2E,
-	MPR121_MHDF            = 0x2F,
-	MPR121_NHDF            = 0x30,
-	MPR121_NCLF            = 0x31,
-	MPR121_FDLF            = 0x32,
-	MPR121_NHDT            = 0x33,
-	MPR121_NCLT            = 0x34,
-	MPR121_FDLT            = 0x35,
 
-	MPR121_TOUCHTH_0       = 0x41,
-	MPR121_RELEASETH_0     = 0x42,
-	MPR121_DEBOUNCE        = 0x5B,
-	MPR121_CONFIG1         = 0x5C,
-	MPR121_CONFIG2         = 0x5D,
-	--MPR121_CHARGECURR_0    = 0x5F,
-	--MPR121_CHARGETIME_1    = 0x6C,
-	MPR121_ECR             = 0x5E,
-	--MPR121_AUTOCONFIG0     = 0x7B,
-	--MPR121_AUTOCONFIG1     = 0x7C,
-	--MPR121_UPLIMIT         = 0x7D,
-	--MPR121_LOWLIMIT        = 0x7E,
-	--MPR121_TARGETLIMIT     = 0x7F,
 
-	--MPR121_GPIODIR         = 0x76,
-	--MPR121_GPIOEN          = 0x77,
-	--MPR121_GPIOSET         = 0x78,
-	--MPR121_GPIOCLR         = 0x79,
-	--MPR121_GPIOTOGGLE      = 0x7A,
-
-	MPR121_SOFTRESET       = 0x80,
-}
 function MPR121:writeRegister(reg,val)
 	print("Write")
     i2c.start(0)
@@ -52,69 +14,102 @@ function MPR121:writeRegister(reg,val)
 	i2c.write(0,val)
     i2c.stop(0)
 end
-
-function MPR121:begin()
+function MPR121:setThresholds(touch, release)
+  for i=0,11 do
+    MPR121:writeRegister(MPR121_TOUCHTH_0 + 2*i, touch)
+    MPR121:writeRegister(MPR121_RELEASETH_0 + 2*i, release)
+  end
+end
+function MPR121:begin(_trig_pin)
 	i2c.setup(0, 2, 1, i2c.SLOW)
     MPR121:writeRegister(codes.MPR121_SOFTRESET, 0x63)
-	tmr.delay(1000)
-    MPR121:writeRegister(codes.MPR121_ECR, 0x0)
+    tmr.delay(1000)
+    MPR121:writeRegister(0x5E, 0x0)
     --local c =readRegister(codes.MPR121_CONFIG2, 1)
     --if (c ~= 0x24) then return false end
     MPR121:setThresholds(12, 6)
-    MPR121:writeRegister(codes.MPR121_MHDR, 0x01)
-    MPR121:writeRegister(codes.MPR121_NHDR, 0x01)
-    MPR121:writeRegister(codes.MPR121_NCLR, 0x0E)
-    MPR121:writeRegister(codes.MPR121_FDLR, 0x00)
-    MPR121:writeRegister(codes.MPR121_MHDF, 0x01)
-    MPR121:writeRegister(codes.MPR121_NHDF, 0x05)
-    MPR121:writeRegister(codes.MPR121_NCLF, 0x01)
-    MPR121:writeRegister(codes.MPR121_FDLF, 0x00)
-    MPR121:writeRegister(codes.MPR121_NHDT, 0x00)
-    MPR121:writeRegister(codes.MPR121_NCLT, 0x00)
-    MPR121:writeRegister(codes.MPR121_FDLT, 0x00)
-    MPR121:writeRegister(codes.MPR121_DEBOUNCE, 0)
-    MPR121:writeRegister(codes.MPR121_CONFIG1, 0x10)
-    MPR121:writeRegister(codes.MPR121_CONFIG2, 0x20)
-    MPR121:writeRegister(codes.MPR121_ECR, 0x8F)
-
+    MPR121:writeRegister(0x2B, 0x01) --MPR121_MHDR
+    MPR121:writeRegister(0x2C, 0x01)--MPR121_NHDR
+    MPR121:writeRegister(0x2D, 0x0E)--MPR121_NCLR
+    MPR121:writeRegister(0x2E, 0x00)--MPR121_FDLR
+    MPR121:writeRegister(0x2F, 0x01)--MPR121_MHDF
+    MPR121:writeRegister(0x30, 0x05)--MPR121_NHDF
+    MPR121:writeRegister(0x31, 0x01)--MPR121_NCLF
+    MPR121:writeRegister(0x32, 0x00)--MPR121_FDLF
+    MPR121:writeRegister(0x33, 0x00)--MPR121_NHDT
+    MPR121:writeRegister(0x34, 0x00)--MPR121_NCLT
+    MPR121:writeRegister(0x35, 0x00)--MPR121_FDLT
+    MPR121:writeRegister(0x5B, 0)--MPR121_DEBOUNCE
+    MPR121:writeRegister(0x5C, 0x10)--MPR121_CONFIG1
+    MPR121:writeRegister(0x5D, 0x20)--MPR121_CONFIG2
+    MPR121:writeRegister(0x5E, 0x8F)--MPR121_ECR
+	--end of setup 
+    old_dataH = 0 
+    old_dataL = 0 
+	if _trig_pin ~= nil then 
+		gpio.mode(_trig_pin, gpio.INT)
+		gpio.trig(_trig_pin, "down", MPR121:update)
+	else 
+		tmr.create():alarm(1000, tmr.ALARM_AUTO, MPR121:update) --no irq pin setup
+	end
 end
+function MPR121:update()
+	i2c.start(0)
+	i2c.address(0 , 0x5A , i2c.TRANSMITTER)
+	i2c.write(0, 0x00)
+	i2c.stop(0)
+	i2c.start(0)
+	i2c.address(0, 0x5A , i2c.RECEIVER)
+	local str = i2c.read(0,2)
+	i2c.stop(0)
+	local b = {}
+	str:gsub(".",function(c) table.insert(b,c) end)
 
-function MPR121:setThresholds(touch, release)
-  for i=0,11 do
-    MPR121:writeRegister(codes.MPR121_TOUCHTH_0 + 2*i, touch)
-    MPR121:writeRegister(codes.MPR121_RELEASETH_0 + 2*i, release)
-  end
+	dataH = string.byte(b[1])
+	dataL = string.byte(b[2])
+
+	MPR121:trig_event()
+
+	old_dataH = dataH
+	old_dataL = dataL
+end
+function MPR121:trig_event()
+
+
 end
 
 --[[
 function MPR121:filteredData(t)
   if (t > 12) then return 0 end
-  return self.reg:r(codes.MPR121_FILTDATA_0L + t*2)
+  return self.reg:r(MPR121_FILTDATA_0L + t*2)
 end
-
 function MPR121:baselineData(t)
   if (t > 12) then return 0 end
-  bl = self.reg:r(codes.MPR121_BASELINE_0 + t, 1)
+  bl = self.reg:r(MPR121_BASELINE_0 + t, 1)
   return bit.lshift(bl, 2)
 end
 ]]--
-function MPR121:readRegister16(reg,length)
-	i2c.start(0)
-    i2c.address(0, 0x5A,i2c.TRANSMITTER)
-    i2c.write(0,reg)
-    i2c.stop(0)
-	i2c.start(0)
-	local error = i2c.address(0, 0x5A, i2c.RECEIVER)
-	if error == true then print("Read Suz") end
-	str = i2c.read(0,2)
-	i2c.stop(0)
-	
-	return tonumber(str)
 
+
+
+function MPR121:whenTouch (_channel)
+    _channel = _channel - 1
+    if _channel < 8 then 
+        if bit.isset(dataH, _channel) == true and bit.isclear(old_dataH,_channel) == true then  return true end
+    elseif _channel > 7 then 
+        if bit.isset(dataL, _channel - 8 ) == true and bit.isclear(old_dataL,_channel - 8) == true then return true end
+    end
+    return false
 end
-function MPR121:touched()
-  t = MPR121:readRegister16(codes.MPR121_TOUCHSTATUS_L, 2)
-  return bit.band(t, 0x0FFF)
+function MPR121:whenRelease(_channel)
+    _channel = _channel - 1
+    if _channel < 8 then 
+        if bit.isset(old_dataH, _channel) == true and bit.isclear(dataH,_channel) == true then  return true end
+    elseif _channel > 7 then 
+        if bit.isset(old_dataL, _channel - 8 ) == true and bit.isclear(dataL,_channel - 8) == true then return true end
+    end
+    return false
+
 end
 
 return MPR121
